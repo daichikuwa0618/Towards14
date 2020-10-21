5202fb6bbfff07a000d9e7d1e682eb345c56b665
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-10-21 18:52:49 +0900

    add: NavigationController を追加、 MainView に TableView を追加

--- Towards14/Towards14/SceneDelegate.swift
+++ Towards14/Towards14/SceneDelegate.swift
@@ -11,13 +11,22 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let mainViewController = storyboard.instantiateInitialViewController() as! ViewController
+
+        navigationController = UINavigationController(rootViewController: mainViewController)
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
